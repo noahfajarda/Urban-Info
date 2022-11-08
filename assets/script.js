6ed133de65c26d18b3a0a2db35b7df76b2f0c4d9
Brandon <brandonkly@ucla.edu>
2022-11-07 20:02:34 -0800

    added search functionality

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
+var userInput = document.createElement("input")
+document.body.appendChild(userInput)
+userInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        searchCity(event.target.value)
+    } 
+})
+
 function callCity(search) {
     return fetch(`https://api.teleport.org/api/cities/?search=${search}`)
 }
-
-callCity()
-.then(response => response.json())
-.then(data => {
+function searchCity(city) {
+    callCity(city)
+    .then(response => response.json())
+    .then(data => {
     console.log(data)
-})
+    })
+}
 console.log("test")
