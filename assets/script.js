b4d5fd4ccabfa812af744a8975e0e778c688b78e
Noah Fajarda <noahfajarda@gmail.com>
2022-11-07 21:49:15 -0800

    retrieved data & salaries from areas

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,42 @@
+// add input element to page
+var userInput = document.createElement("input");
+document.body.appendChild(userInput);
+// listen for user keyboard input
+userInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        // search for city upon enter
+        searchCity(event.target.value);
+    }
+});
+
 function callCity(search) {
-    return fetch(`https://api.teleport.org/api/cities/?search=${search}`)
+    return fetch(`https://api.teleport.org/api/cities/?search=${search}`);
+}
+function searchCity(city) {
+    // fetch api using 'callCity' function
+    city = city.replaceAll(" ", "-");
+    console.log(city);
+    cityData(city)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+
+    citySalaries(city)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
 }
 
-callCity()
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
-console.log("test")
+function cityData(area) {
+    return fetch(
+        `https://api.teleport.org/api/urban_areas/slug:${area}/details/`
+    );
+}
+
+function citySalaries(area) {
+    return fetch(
+        `https://api.teleport.org/api/urban_areas/slug:${area}/salaries/`
+    );
+}
