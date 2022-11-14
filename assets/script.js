5fc082ff9686eb3c9bde816062985ca7c8c0c658
Lsenn1 <lsenn@lion.lmu.edu>
2022-11-14 14:12:31 -0800

    added special suprise

--- assets/script.js
+++ assets/script.js
@@ -701,3 +701,18 @@ buttons.forEach((button) => {
         }
     });
 });
+
+Array.from(document.getElementsByClassName('showmodal')).forEach( (e) => {
+    e.addEventListener('click', function(element) {
+      element.preventDefault();
+      if (e.hasAttribute('data-show-modal')) {
+        showModal(e.getAttribute('data-show-modal'));
+      }
+    }); 
+  });
+  // Show modal dialog
+  function showModal(modal) {
+    const mid = document.getElementById(modal);
+    let myModal = new bootstrap.Modal(mid);
+    myModal.show();
+  }
